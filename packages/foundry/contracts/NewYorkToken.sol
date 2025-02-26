5b672c6974fbec7c58c254fdd8fd7525290b706a
zayman2045 <xavierorlandogriffith@gmail.com>
2025-02-26 06:55:51 -0500

    Add airdropTokens function to NewYorkToken contract for token distribution

--- packages/foundry/contracts/NewYorkToken.sol
+++ packages/foundry/contracts/NewYorkToken.sol
@@ -6,6 +6,11 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NewYorkToken is ERC20, Ownable {
-    
+
     constructor() ERC20("NewYorkToken", "NYT") Ownable(msg.sender) {}
+
+    function airdropTokens(uint256 amount) public {
+        _mint(msg.sender, amount);
+        console.log("Airdropped", amount, "NYT tokens to", msg.sender);
+    }
 }
